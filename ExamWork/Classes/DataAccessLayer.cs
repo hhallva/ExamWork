6fff6fd1950ca2f940d732ec926e77c8017bb891
Вячеслав <slavvva05@gmail.com>
2024-06-18 19:37:18 +0300

    Проверка на существование пользователя
    Добавил метод на проверку существует ли пользователь в БД. И использовал его

--- ExamWork/Classes/DataAccessLayer.cs
+++ ExamWork/Classes/DataAccessLayer.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace ExamWork.Classes
 {
@@ -12,7 +14,7 @@ namespace ExamWork.Classes
         #region Свойства
         public static string ServerName { get; set; } = @"MSI";
         public static string DatabaseName { get; set; } = "Exam";
-        public static string Login { get; set; } = "";
+        public static string Login { get; set; } = @"MSI\slavv";
         public static string Password { get; set; } = "";
         public static string ConnectionString
         {
@@ -21,9 +23,10 @@ namespace ExamWork.Classes
                 SqlConnectionStringBuilder builder = new()
                 {
                     DataSource = ServerName, 
-                    //UserID = Login,
+                    UserID = Login,
                     //Password = Password,
                     InitialCatalog = DatabaseName,
+                    IntegratedSecurity = true,
                     TrustServerCertificate = true
                 };
                 return builder.ConnectionString;
@@ -31,18 +34,18 @@ namespace ExamWork.Classes
         }
         #endregion
 
-        public static bool IsUser(string login, string password)
+        public static bool IsUserExist(string login, string password)
         {
-            using SqlConnection connection = new(ConnectionString);
+            SqlConnection connection = new(ConnectionString);
             connection.Open();
 
             string query = "SELECT * FROM ExamUser WHERE UserLogin = @login AND UserPassword = @password";
 
-            using SqlCommand command = new(query, connection);
+            SqlCommand command = new(query, connection);
             command.Parameters.AddWithValue("@login", login);
             command.Parameters.AddWithValue("@password", password);
 
-            return command.ExecuteNonQuery() > 0;
+            return command.ExecuteScalar() != null;
         }
     }
 }
--- ExamWork/Pages/AuthorizationPage.xaml.cs
+++ ExamWork/Pages/AuthorizationPage.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using ExamWork.Classes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ExamWork.Pages
 {
@@ -28,17 +16,16 @@ namespace ExamWork.Pages
 
         private void AuthorizationButton_Click(object sender, RoutedEventArgs e)
         {
-            
-            //проверка существует ли такой пользователь в бд 
-            if (DataAccessLayer.IsUser(loginTextBox.Text, passwordBox.Password))
+            if (DataAccessLayer.IsUserExist(loginTextBox.Text, passwordBox.Password))
             {
-                MessageBox.Show("ок");
-                //всё супер пропускаем
+                App.CurrentFrame.Navigate(new ShopPage());
             }
-            else  
+            else
             {
-                MessageBox.Show("не ок");
-                //всё плохо сообщение о том что нет такого пользователя
+                MessageBox.Show("Пользователь не может быть авторизован. \nЛогин или пароль введены неверно.",
+                                "Сообщение",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error);
             }
         }
 
